3ea5939808bb129f6732b66bfa53a3819988f3f7
Yeonguk <choo121600@gmail.com>
2026-01-25 13:24:08 +0900

    fixup!fixup!

--- plugin.rb
+++ plugin.rb
@@ -11,141 +11,134 @@ enabled_site_setting :custom_webhook_enabled
 
 register_asset "stylesheets/custom-webhook-admin.scss"
 
-module ::DiscourseCustomWebhook
-  PLUGIN_NAME = "discourse-custom-webhook"
+after_initialize do
+  module ::DiscourseCustomWebhook
+    PLUGIN_NAME = "discourse-custom-webhook"
 
-  class Engine < ::Rails::Engine
-    engine_name PLUGIN_NAME
-    isolate_namespace DiscourseCustomWebhook
-  end
-
-  def self.ensure_protocol(url)
-    return nil if url.blank?
-    url.start_with?("//") ? "https:#{url}" : url
-  end
-
-  def self.formatted_display_name(user)
-    if user.name.present? && user.name != user.username
-      "@#{user.username} (#{user.name})"
-    else
-      "@#{user.username}"
-    end
-  end
-
-  def self.build_payload(channel, post)
-    topic = post.topic
-    user = post.user
-    category = topic.category
-
-    # Build title with category and tags
-    title_parts = [topic.title]
-
-    if category.present?
-      category_name = category.parent_category.present? ? "#{category.parent_category.name}/#{category.name}" : category.name
-      title_parts << "[#{category_name}]"
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseCustomWebhook
     end
 
-    tags = topic.tags.pluck(:name)
-    title_parts << tags.join(", ") if tags.present?
-
-    title = title_parts.join(" ")
-
-    # Get category color
-    embed_color = if category&.color.present?
-      category.color.to_i(16)
-    else
-      5793266
+    def self.ensure_protocol(url)
+      return nil if url.blank?
+      url.start_with?("//") ? "https:#{url}" : url
     end
 
-    # Build author info
-    base_url = Discourse.base_url
-    author_url = "#{base_url}/u/#{user.username}"
-    avatar_url = ensure_protocol(user.small_avatar_url)
-
-    excerpt_length = channel.excerpt_length || 400
-
-    {
-      content: channel.message_content.presence || "",
-      embeds: [
-        {
-          title: title,
-          color: embed_color,
-          description: post.excerpt(excerpt_length, keep_newlines: true, keep_emoji_images: false) || "",
-          url: post.full_url,
-          author: {
-            name: formatted_display_name(user),
-            url: author_url,
-            icon_url: avatar_url
-          }
-        }
-      ]
-    }
-  end
-
-  def self.send_to_channel(channel, post)
-    payload = build_payload(channel, post)
-
-    uri = URI.parse(channel.webhook_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme == "https"
-    http.open_timeout = 5
-    http.read_timeout = 10
-
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request["Content-Type"] = "application/json"
-
-    if channel.webhook_secret.present?
-      request["X-Webhook-Secret"] = channel.webhook_secret
-    end
-
-    request.body = payload.to_json
-
-    response = http.request(request)
-
-    unless response.is_a?(Net::HTTPSuccess)
-      Rails.logger.warn("[CustomWebhook] Channel '#{channel.name}' failed: #{response.code} - #{response.body}")
-      raise "HTTP #{response.code}: #{response.body}"
-    end
-
-    response
-  end
-
-  def self.send_notification(post)
-    return unless SiteSetting.custom_webhook_enabled
-    return if post.post_type != Post.types[:regular]
-    return if post.topic.blank? || post.topic.archetype == Archetype.private_message
-
-    # Find all channels with matching rules
-    matching_channels = []
-
-    DiscourseCustomWebhook::CustomWebhookChannel.enabled.includes(:rules).find_each do |channel|
-      # Check if any rule matches this post (excluding muted rules)
-      matching_rule = channel.rules.ordered.find { |rule| rule.matches_post?(post) }
-
-      if matching_rule
-        matching_channels << channel
-      elsif channel.rules.empty?
-        # Channel with no rules = catch-all (watch all)
-        matching_channels << channel
+    def self.formatted_display_name(user)
+      if user.name.present? && user.name != user.username
+        "@#{user.username} (#{user.name})"
+      else
+        "@#{user.username}"
       end
     end
 
-    return if matching_channels.empty?
+    def self.build_payload(channel, post)
+      topic = post.topic
+      user = post.user
+      category = topic.category
 
-    # Send to all matching channels in background threads
-    matching_channels.each do |channel|
-      Thread.new do
-        begin
-          send_to_channel(channel, post)
-        rescue => e
-          Rails.logger.error("[CustomWebhook] Error sending to '#{channel.name}': #{e.message}")
+      title_parts = [topic.title]
+
+      if category.present?
+        category_name = category.parent_category.present? ? "#{category.parent_category.name}/#{category.name}" : category.name
+        title_parts << "[#{category_name}]"
+      end
+
+      tags = topic.tags.pluck(:name)
+      title_parts << tags.join(", ") if tags.present?
+
+      title = title_parts.join(" ")
+
+      embed_color = if category&.color.present?
+        category.color.to_i(16)
+      else
+        5793266
+      end
+
+      base_url = Discourse.base_url
+      author_url = "#{base_url}/u/#{user.username}"
+      avatar_url = ensure_protocol(user.small_avatar_url)
+
+      excerpt_length = channel.excerpt_length || 400
+
+      {
+        content: channel.message_content.presence || "",
+        embeds: [
+          {
+            title: title,
+            color: embed_color,
+            description: post.excerpt(excerpt_length, keep_newlines: true, keep_emoji_images: false) || "",
+            url: post.full_url,
+            author: {
+              name: formatted_display_name(user),
+              url: author_url,
+              icon_url: avatar_url
+            }
+          }
+        ]
+      }
+    end
+
+    def self.send_to_channel(channel, post)
+      payload = build_payload(channel, post)
+
+      uri = URI.parse(channel.webhook_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == "https"
+      http.open_timeout = 5
+      http.read_timeout = 10
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request["Content-Type"] = "application/json"
+
+      if channel.webhook_secret.present?
+        request["X-Webhook-Secret"] = channel.webhook_secret
+      end
+
+      request.body = payload.to_json
+
+      response = http.request(request)
+
+      unless response.is_a?(Net::HTTPSuccess)
+        Rails.logger.warn("[CustomWebhook] Channel '#{channel.name}' failed: #{response.code} - #{response.body}")
+        raise "HTTP #{response.code}: #{response.body}"
+      end
+
+      response
+    end
+
+    def self.send_notification(post)
+      return unless SiteSetting.custom_webhook_enabled
+      return if post.post_type != Post.types[:regular]
+      return if post.topic.blank? || post.topic.archetype == Archetype.private_message
+
+      matching_channels = []
+
+      DiscourseCustomWebhook::CustomWebhookChannel.enabled.includes(:rules).find_each do |channel|
+        matching_rule = channel.rules.ordered.find { |rule| rule.matches_post?(post) }
+
+        if matching_rule
+          matching_channels << channel
+        elsif channel.rules.empty?
+          matching_channels << channel
+        end
+      end
+
+      return if matching_channels.empty?
+
+      matching_channels.each do |channel|
+        Thread.new do
+          begin
+            send_to_channel(channel, post)
+          rescue => e
+            Rails.logger.error("[CustomWebhook] Error sending to '#{channel.name}': #{e.message}")
+          end
         end
       end
     end
   end
-end
 
-after_initialize do
   # Load models
   require_relative "app/models/custom_webhook_channel"
   require_relative "app/models/custom_webhook_rule"
@@ -158,16 +151,23 @@ after_initialize do
   require_relative "app/controllers/custom_webhook_channels_controller"
   require_relative "app/controllers/custom_webhook_rules_controller"
 
-  # Load routes
-  require_relative "config/routes"
-
   # Register admin route
   add_admin_route "custom_webhook.title", "custom-webhook"
 
-  # Extend AdminPluginsController to add our route
+  # Mount engine routes
   Discourse::Application.routes.append do
+    mount ::DiscourseCustomWebhook::Engine, at: "/admin/plugins/custom-webhook"
     get "/admin/plugins/custom-webhook" => "admin/plugins#index", constraints: StaffConstraint.new
     get "/admin/plugins/custom-webhook/*path" => "admin/plugins#index", constraints: StaffConstraint.new
+  end
+
+  DiscourseCustomWebhook::Engine.routes.draw do
+    resources :channels, controller: "custom_webhook_channels", except: [:new, :edit] do
+      member do
+        post :test
+      end
+      resources :rules, controller: "custom_webhook_rules", only: [:create, :update, :destroy]
+    end
   end
 
   on(:post_created) do |post, opts, user|
@@ -180,8 +180,7 @@ after_initialize do
     end
   end
 
-  # Job for delayed notifications
-  module Jobs
+  module ::Jobs
     class CustomWebhookNotify < ::Jobs::Base
       def execute(args)
         return unless args[:post_id]
